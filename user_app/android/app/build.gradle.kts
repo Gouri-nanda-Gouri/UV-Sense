6d045d24b2ff2b9731abc27bbd7cbfbfaafcdade
Gouri-nanda-Gouri <nandagouri03@gmail.com>
2026-03-18 10:14:01 +0530

    Build Completed

--- user_app/android/app/build.gradle.kts
+++ user_app/android/app/build.gradle.kts
@@ -7,15 +7,8 @@ plugins {
 
 android {
     namespace = "com.example.user_app"
-    
-    // Set to 36 to satisfy plugin requirements
-    compileSdk = 36 
-    
-    // Matches your installed NDK version
-    ndkVersion = "27.0.12077973" 
-    
-    // Matches your installed Build-Tools version
-    buildToolsVersion = "36.1.0" 
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -27,20 +20,19 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID.
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.user_app"
-        
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        
-        // Matches compileSdk to ensure compatibility
-        targetSdk = 36 
-        
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
+            // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
